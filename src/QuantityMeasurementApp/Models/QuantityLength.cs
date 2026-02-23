58099da8aed9e1b6065af0e47d70591fd8f30738
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2026-02-24 01:30:49 +0530

    Vansh Tyagi.Add - Added UC5 conversion API and menu conversion option

--- src/QuantityMeasurementApp/Models/QuantityLength.cs
+++ src/QuantityMeasurementApp/Models/QuantityLength.cs
@@ -83,6 +83,52 @@ namespace QuantityMeasurementApp.Models
             return $"Quantity({formattedValue}, \"{unitText}\")";
         }
 
+        public static double Convert(double measurementValue, LengthUnit? sourceUnit, LengthUnit? targetUnit)
+        {
+            if (double.IsNaN(measurementValue) || double.IsInfinity(measurementValue))
+            {
+                throw new ArgumentException("Length value must be a finite number.", nameof(measurementValue));
+            }
+
+            if (sourceUnit is null)
+            {
+                throw new ArgumentNullException(nameof(sourceUnit), "Source unit cannot be null.");
+            }
+
+            if (targetUnit is null)
+            {
+                throw new ArgumentNullException(nameof(targetUnit), "Target unit cannot be null.");
+            }
+
+            if (!Enum.IsDefined(typeof(LengthUnit), sourceUnit.Value))
+            {
+                throw new ArgumentException("Unsupported source length unit.", nameof(sourceUnit));
+            }
+
+            if (!Enum.IsDefined(typeof(LengthUnit), targetUnit.Value))
+            {
+                throw new ArgumentException("Unsupported target length unit.", nameof(targetUnit));
+            }
+
+            double sourceFactorToInches = sourceUnit.Value.GetConversionFactorToInches();
+            double targetFactorToInches = targetUnit.Value.GetConversionFactorToInches();
+
+            // Convert using the common base unit (inches):
+            // result = value × (sourceFactor / targetFactor)
+            return measurementValue * (sourceFactorToInches / targetFactorToInches);
+        }
+
+        public QuantityLength ConvertTo(LengthUnit targetUnit)
+        {
+            if (!Enum.IsDefined(typeof(LengthUnit), targetUnit))
+            {
+                throw new ArgumentException("Unsupported target length unit.", nameof(targetUnit));
+            }
+
+            double convertedValue = Convert(measurementValue, lengthUnit, targetUnit);
+            return new QuantityLength(convertedValue, targetUnit);
+        }
+
         private double ConvertToInches()
         {
             double conversionFactorToInches = lengthUnit.GetConversionFactorToInches();
