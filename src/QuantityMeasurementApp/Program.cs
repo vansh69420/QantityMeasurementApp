b6e4a401457ab4481ca19bac59d347d198b3637b
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2026-03-09 16:29:57 +0530

    Vansh Tyagi.Refactor - Removed console menu in favor of Swagger Web API (UC15)

--- src/QuantityMeasurementApp/Program.cs
+++ src/QuantityMeasurementApp/Program.cs
@@ -1,20 +1,10 @@
-﻿using ControllerLayer.Controllers;
-using RepositoryLayer.Repositories;
-using ServiceLayer.Services;
-using QuantityMeasurementApp.Menu;
-
-namespace QuantityMeasurementApp
+﻿namespace QuantityMeasurementApp
 {
     internal static class Program
     {
         private static void Main()
         {
-            QuantityMeasurementCacheRepository repository = QuantityMeasurementCacheRepository.Instance;
-            QuantityMeasurementServiceImpl service = new QuantityMeasurementServiceImpl(repository);
-            QuantityMeasurementController controller = new QuantityMeasurementController(service);
-
-            QuantityMeasurementMenu quantityMeasurementMenu = new QuantityMeasurementMenu(controller);
-            quantityMeasurementMenu.Run();
+            
         }
     }
 }
